1df926511b51ac61e96a01e389b0995845e57fc2
haquoccuong <cuonghq1.longan@gmail.com>
2021-12-10 22:40:19 +0700

    update

--- src/main/java/com/se/webbanhang/WebbanhangonlineServerApplication.java
+++ src/main/java/com/se/webbanhang/WebbanhangonlineServerApplication.java
@@ -9,5 +9,5 @@ public class WebbanhangonlineServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebbanhangonlineServerApplication.class, args);        
 	}
-
+        
 }
---
+++ src/main/java/com/se/webbanhang/config/CorsConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.se.webbanhang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Admin
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3006");
+            }
+        };
+    }
+}
