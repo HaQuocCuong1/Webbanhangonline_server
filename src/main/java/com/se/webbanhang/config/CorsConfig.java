44452373941f6f5bd2b55d8b1740683a713128ef
haquoccuong <cuonghq1.longan@gmail.com>
2021-12-15 22:26:30 +0700

    update

--- src/main/java/com/se/webbanhang/config/CorsConfig.java
+++ src/main/java/com/se/webbanhang/config/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3006", "https://shop-cookies.netlify.app");
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3006", "https://shop-cookies.netlify.app", "https://cookiesshop-manager.netlify.app");
             }
         };
     }
