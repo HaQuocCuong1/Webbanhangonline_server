9a3f1e166a05846926bfbf7aa2af85cfa0e3dbe7
haquoccuong <cuonghq1.longan@gmail.com>
2021-12-27 03:34:10 +0700

    update

--- src/main/java/com/se/webbanhang/service/OrderServiceImpl.java
+++ src/main/java/com/se/webbanhang/service/OrderServiceImpl.java
@@ -8,6 +8,7 @@ package com.se.webbanhang.service;
 import com.se.webbanhang.entity.Order_detail;
 import com.se.webbanhang.repository.OrderRespository;
 import com.se.webbanhang.entity.Orders;
+import com.se.webbanhang.entity.Products;
 import com.se.webbanhang.entity.Users;
 import com.se.webbanhang.exception.NotFoundException;
 import java.io.UnsupportedEncodingException;
@@ -72,13 +73,98 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public boolean updateStatusOrder(int id) {
        Orders theOrders = findbyId(id);
+       List<Order_detail> listOd = theOrders.getListOrderDetail();
+       Users theUsers2 = theOrders.getUsers();
        if(theOrders == null)
            return false;
        else
        {
            oderRespository.updateStatusOrder(theOrders.getId());
+           for(Order_detail od : listOd)
+           {
+               if(od != null)
+               {
+                   Products p = od.getProducts();
+                   Users theUsers1 = p.getUser();
+                   try {
+                       sendReceiveOrder(theUsers1, theUsers2, theOrders);
+                   } catch (MessagingException ex) {
+                       Logger.getLogger(OrderServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+                   } catch (UnsupportedEncodingException ex) {
+                       Logger.getLogger(OrderServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+                   }
+               }
+           }
            return true;
        }
+    }
+    public void sendReceiveOrder(Users theUsers1, Users theUsers2, Orders theOrders) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Thông tin nhận đơn hàng của bạn";
+        String senderName = "web ban hang online";
+        String mailContent = "<p>Hi "+ theUsers1.getName()+ ",</p>";
+        mailContent += "<p> Chúc mừng bạn, sản phẩm của bạn đã được một ai đó mua:</p>";
+        mailContent += "<h3> THÔNG TIN ĐƠN HÀNG</h3>";
+        mailContent += "<p> Mã đơn hàng: "+ theOrders.getId() +"</p>";
+        mailContent += "<p> Dịch vụ: ĐẶT HÀNG TRỰC TUYẾN </p>";
+        mailContent += "<h3> THÔNG TIN NGƯỜI NHẬN</h3>";
+        mailContent += "<p> Tên người nhận: "+ theOrders.getNamecustomer()+"</p>";
+        mailContent += "<p> Email: "+ theOrders.getEmail()+"</p>";
+        mailContent += "<p> Địa chỉ mua hàng: "+ theOrders.getDeliveryaddress()+"</p>";
+        mailContent += "<p> Số điện thoại: "+ theOrders.getPhone()+"</p>";
+        int payments = theOrders.getPayments();
+        if (payments == 1)
+        {
+            mailContent += "<p> Hình thức thanh toán: TIỀN MẶT</p>";
+        }
+        if (payments == 0)
+        {
+            mailContent += "<p> Hình thức thanh toán: ONLINE</p>";
+        }
+        mailContent += "<h3> SẢN PHẨM ĐÃ ĐẶT</h3>";
+        mailContent += "<table style='width: 600px'>";
+        mailContent += "<thead>";
+        mailContent += "<th>Sản phẩm</th>";
+        mailContent += "<th>Số lượng</th>";
+        mailContent += "<th>Giá tiền</th>";
+        mailContent += "</thead>";
+        mailContent += "<tbody>";
+        double total = 0;
+        
+        for(Order_detail od : theOrders.getListOrderDetail())
+        {
+            if(od.getProducts().getUser().getId() == theUsers1.getId())
+            {
+                mailContent += "<tr>";
+                mailContent += "<td>"+ od.getName()+ "</td>";
+                mailContent += "<td>"+ od.getQuantity() +"</td>";
+                mailContent += "<td>"+ od.getTotalmoney() + "</td>";
+                mailContent += "</tr>";
+                total += od.getTotalmoney();
+            }
+        }
+        mailContent += "<tr>";
+        mailContent += "<td></td>";
+        mailContent += "<td>Tổng tiền thanh toán:</td>";
+        mailContent += "<td>"+ total + "vnd</td>";
+        mailContent += "</tr>";
+        mailContent += "</tbody>";
+        mailContent += "</table>";
+        mailContent += "<p> Mọi chi tiết xin liên hệ qua số điện thoại: 0374858374, Xin cảm ơn quý khách đã sử dụng dịch vụ của chúng tôi:</p>";
+        
+        mailContent += "<p> Thank you <br/> Web ban hang online</p>";
+        try
+        {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper hepHelper = new MimeMessageHelper(message, "utf-8");
+            hepHelper.setFrom("hacuong6212@gmail.com", senderName);
+            hepHelper.setTo(theUsers1.getEmail());
+            hepHelper.setSubject(subject);
+            hepHelper.setText(mailContent, true);
+            mailSender.send(message);
+        } catch(MessagingException e)
+        {
+           throw new IllegalStateException("failed to send mail", e);
+        }
     }
     public void sendVerificationOrderEmail(Orders theOrders) throws MessagingException, UnsupportedEncodingException {
         String subject = "Thông tin đơn hàng của bạn";
